10af2bc00f8787f9b185ccfe5a62cdb21137d0a4
naman <namankhetwal99@gmail.com>
2025-04-28 16:06:01 +0530

    Initialize react on the system and type hello-world

---
+++ script.js
@@ -0,0 +1,33 @@
+/*What is react?
+open source library for building user interfaces
+Not a framework 
+focus on UI
+Rich ecosystem
+
+Why learn React?
+Created and maintained by Facebook 
+More than 100k starts on Github
+Huge community 
+In demand skillset
+
+Component Based Architecture
+
+Resuable code
+
+React is declarative
+tell React what you want and React will build the actual UI
+React will handle efficiently updating and rendering of the components
+DOM updates are handle gracefully in React
+
+More on why REAcT?
+Seamlessly integrate react into any of your application
+Portion of your page a complete page or even an entire application itself 
+React native for mobile applications
+
+Prerequistes
+HTML,CSS and Javascript fundamentals
+ES6
+JavaScript - 'this' keyword , filter, map and reduce
+ES6 - let & const , arrow function , template literals , default parameters , object literals , rest and spread operators and destructuring assignment.
+
+*/
