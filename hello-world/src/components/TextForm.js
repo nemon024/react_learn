29c27005c02406f45ce1ff668ca5263d4f82a712
naman <namankhetwal99@gmail.com>
2025-05-08 14:16:52 +0530

    Add dark mode functionality to Navbar and TextForm components #Improving Dark Mode & Refactoring App component

--- hello-world/src/components/TextForm.js
+++ hello-world/src/components/TextForm.js
@@ -39,10 +39,10 @@ export default function TextForm(props) {
   // setText('new Text'); // Correct way to change the state
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control" value={text}onChange={handleOnChanged} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text}onChange={handleOnChanged} style={{background: props.mode === 'dark' ? 'grey' : 'white', color:props.mode === 'dark' ? 'white' : 'black'}} id="myBox" rows="8"></textarea>
       </div>
       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to upper case</button>
       <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lower case</button>
@@ -50,12 +50,12 @@ export default function TextForm(props) {
       <button className="btn btn-primary mx-2" onClick={handleSentenceClick}>Convert To Sentence</button>
       <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy the value</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
       <h2>Your text summary</h2>
       <p>{text.split(" ").length} words , {text.length} characters</p>
       <p>{(0.008 * text.split(" ").length).toFixed(2)} Minutes read</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length >0 ? text: 'Enter something in the textbox above to preview it here' }</p>
 
     </div>
     </>
